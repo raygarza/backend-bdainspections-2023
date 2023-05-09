8a8dc1cb2bcfbd1050fd28d575cbe17dd079c40d
Ray Garza <raydev@protonmail.com>
2023-05-09 13:47:27 -0500

    server.js fixz

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,21 +1,16 @@
-// module.exports = ({ env }) => ({
-//     proxy: true,
-//     host: "0.0.0.0",
-//     port: process.env.PORT,
-//     url: env('MY_HEROKU_URL'),
-//     app: { 
-//       keys: env.array('APP_KEYS')
-//     },
-//     admin: {
-//       auth: {
-//         secret: env('ADMIN_JWT_SECRET'),
-//       },
-//     },
-//   })
-
-
-
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
-});
- 
+    proxy: true,
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    url: env('MY_HEROKU_URL'),
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
+    },
+  })
+
+
