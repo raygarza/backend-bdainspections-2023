af32cd4741f26ec25add085a61730c53f85d7ac7
Ray Garza <raydev@protonmail.com>
2023-05-09 14:04:29 -0500

    server.js fixes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,16 +1,14 @@
 module.exports = ({ env }) => ({
-    proxy: true,
-    host: "0.0.0.0",
-    port: process.env.PORT,
-    url: env('MY_HEROKU_URL'),
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
-    admin: {
-      auth: {
-        secret: env('ADMIN_JWT_SECRET'),
-      },
-    },
+    // proxy: true,
+    // host: "0.0.0.0",
+    // port: process.env.PORT,
+    url: env('https://bdainspections-2023.herokuapp.com/'),
+    // app: { 
+    //   keys: env.array('oGHarJXx3Dtw7NJ1FnvqBg==,d69sYotr5Paho1qVjfVrzw==,X5IxyIJavb8mJ2QIkz9rRg==,GYjl8mpWdpqgR7rhxNelgQ==')
+    // },
+    // admin: {
+    //   auth: {
+    //     secret: env('ADMIN_JWT_SECRET'),
+    //   },
+    // },
   })
-
-
--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  app: { 
+    keys: env.array('oGHarJXx3Dtw7NJ1FnvqBg==,d69sYotr5Paho1qVjfVrzw==,X5IxyIJavb8mJ2QIkz9rRg==,GYjl8mpWdpqgR7rhxNelgQ==')
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
